8a74b29ace76543ee23e43114aa6f83b4ff09d3d
Marcos Pereira <Marcosprr34@gmail.com>
2020-08-21 00:29:46 -0300

    Tratamento de Url amigavel

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php 
+<?php
+            $url = (isset($_GET['url'])) ? $_GET['url']:'Inicio';
+            $url = array_filter(explode('/',$url));
+            $file = 'View/'.$url[0].'.php';
+            if(is_file($file)){
+                include($file);
+            }else{
+                include('View/404.php');
+            }
 
-    echo 'teste';
+        ?>
