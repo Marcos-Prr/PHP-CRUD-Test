5a3c8bdfcb4cb5d73895d22de6b63dc9e5ee1a19
Marcos Pereira <marcosprr34@gmail.com>
2020-08-24 23:41:33 -0300

    sql para funções do CRUD

--- Model/Produtos.php
+++ Model/Produtos.php
@@ -13,4 +13,54 @@
 
             return $Produtos;
         }
+
+        public static function criar($postDados){
+            $con = new PDO('mysql: host=localhost; dbname=crud_produtos;','root','');
+
+            $sqlInsert = $con->prepare('INSERT INTO produtos (Nome,Valor,Descricao,Nome_imagem) VALUES (:Nome,:Valor,:Descricao,:Nome_imagem)');
+            $sqlInsert->bindValue(':Nome', $postDados['Nome']);
+            $sqlInsert->bindValue(':Valor', $postDados['Preco']);
+            $sqlInsert->bindValue(':Descricao', $postDados['Descricao']);
+            $sqlInsert->bindValue(':Nome_imagem', $postDados['imagem']);
+            $sqlInsert->execute();
+        }
+    
+        public static function editar($postDados,$idProduto){
+            $con = new PDO('mysql: host=localhost; dbname=crud_produtos;','root','');
+
+            $sqlEditar = $con->prepare("UPDATE produtos SET Nome = :Nome, Valor = :Valor , Descricao = :Descricao , Nome_imagem = :Nome_imagem WHERE id = :id ");
+            $sqlEditar->bindValue(':Nome', $postDados['Nome']);
+            $sqlEditar->bindValue(':Valor', $postDados['Preco']);
+            $sqlEditar->bindValue(':Descricao', $postDados['Descricao']);
+            $sqlEditar->bindValue(':Nome_imagem', $postDados['imagem']);
+            $sqlEditar->bindValue(':id', $idProduto);
+            $sqlEditar->execute();
+
+        }
+    
+        public static function excluir($idProduto){
+            $con = new PDO('mysql: host=localhost; dbname=crud_produtos;','root','');
+
+            $Produto = Produtos::selecionaPorId($idProduto);
+            $targetDir = "./imagens/". $Produto->Nome_imagem;
+            unlink($targetDir);
+            
+            $sqlDelete = $con->prepare("DELETE FROM produtos WHERE id = :id");
+            $sqlDelete->bindValue(':id', $idProduto);
+            $sqlDelete->execute();
+
+        }
+
+        public static function selecionaPorId($idProduto){
+
+            $con = new PDO('mysql: host=localhost; dbname=crud_produtos;','root','');
+            $convertIdProduto = intval($idProduto);
+            $sqlSeleciona = $con->prepare("SELECT * FROM produtos WHERE id = :id");
+            $sqlSeleciona->bindValue(':id', $convertIdProduto);
+            $sqlSeleciona->execute();
+            $Produto =  $sqlSeleciona->fetchObject('Produtos');
+
+
+            return $Produto;
+        }
     }
